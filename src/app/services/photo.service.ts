ef5a004c78f1e01a23e3f11561cd7568833611ea
Megan Karbowski <megan.karbowski4@gmail.com>
2021-01-24 20:53:51 -0700

    photos are saved to filebase but are being displayed twice?

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
 
-// First, import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins
-
 import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
   CameraPhoto, CameraSource } from '@capacitor/core';
 
 const { Camera, Filesystem, Storage } = Plugins;
 
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
-  // define an array of Photos, which will contain a reference to each photo captured with the Camera.
-
   public photos: Photo[] = [];
 
   constructor() { }
 
-
-  // define a new class method, addNewToGallery, that will contain the core logic to take a 
-  // device photo and save it to the filesystem. Let’s start by opening the device camera:
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
@@ -28,17 +26,52 @@ export class PhotoService {
       quality: 100 
     });
 
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+
     // add the newly captured photo to the beginning of the Photos array.
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath
     });
   }
+  
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+  
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
 
-}
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+  
+    return await this.convertBlobToBase64(blob) as string;  
+  }
+  
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 
-// create a new interface, Photo, to hold our photo metadata:
-export interface Photo {
-  filepath: string;
-  webviewPath: string;
 }
